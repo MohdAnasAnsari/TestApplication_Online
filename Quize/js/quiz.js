2f9bd95ea57b5bdaf5b93fbd6a6a7139728eca28
Mohd Anas Ansari <mohdanan@cisco.com>
2023-02-18 23:53:31 +0530

    first commit

---
+++ Quize/js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"What is the full form of SQL?",
+        option:[
+            "1.Structured Query Language",
+            "2.Structure Query List",
+            "3.Sample Query Language",
+            "4.None of the Above",
+        ],
+        answer:1,
+    },
+    {
+        question:" Which of the following is not a valid SQL type?",
+        option:[
+            "1.FLOAT",
+            "2.NUMERIC",
+            "3.DECIMAL",
+            "4.CHARACTER",
+            
+        ],
+        answer:3,
+    },
+    {
+        question:"Which of the following is not a DDL command?",
+        option:[
+            "1.TRUNCATE",
+            "2.ALTER",
+            "3.CREATE",
+            "4.UPDATE",   
+        ],
+        answer:4,
+    },
+    {
+        question:"Which statement is used to delete all rows in a table without having the action logged?",
+        option:[
+            "1.DELETE",
+            "2.REMOVE ",
+            "3.DROP",
+            "4.TRUNCATE",   
+        ],
+        answer:4,
+
+    },
+    {
+        question:"How many Primary keys can have in a table?",
+        option:[
+            "1.Only 1",
+            "2.Only 2",
+            "3.Depends on no of Columns",
+            "4.Depends on DBA",   
+        ],
+        answer:1,
+    }
+]
